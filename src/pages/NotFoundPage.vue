8f3f37cee9674ddae2156cf11e5abf37c87ba272
scava <galsanov.bul888@gmail.com>
2023-07-02 00:32:02 +0700

    Authentication V2

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="fixed-centered">
+      <h1>404 - Not Found</h1>
+      <p style="margin-top: 5px;">Такой страницы нет.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      content: ''
+    }
+  },
+  mounted () {
+  }
+}
+</script>
