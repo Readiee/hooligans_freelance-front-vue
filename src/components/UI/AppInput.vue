ef2d7f77606b209c5f13be72aa0d97bd99496907
scava <galsanov.bul888@gmail.com>
2023-07-02 21:44:27 +0700

    Components refactoring

--- src/components/UI/AppInput.vue
+++ src/components/UI/AppInput.vue
@@ -21,7 +21,5 @@ export default {
   width: 100%;
   border: 2px solid rgb(187, 84, 32);
   padding: 10px 15px;
-  margin-right: 15px;
-  margin-top: 15px;
 }
 </style>
--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,7 +1,7 @@
 import MyButton from '@/components/UI/AppPrimaryButton'
-import MyInput from '@/components/UI/MyInput'
-import MyDialog from '@/components/UI/MyDialog'
-import MySelect from '@/components/UI/MySelect'
+import MyInput from '@/components/UI/AppInput'
+import MyDialog from '@/components/UI/AppDialog'
+import MySelect from '@/components/UI/AppSelect'
 
 export default [
   MyButton,
